6004d07baeef6b5cebb2a95f17d052c3e222446a
Grihith Manchanda <grihithmanchanda@gmail.com>
2023-01-23 20:20:24 -0500

    Added multi screen stuff

---
+++ src/screens/UserHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+function UserHome() {
+    return (
+        <View>
+            <Text>Word</Text>
+        </View>
+    );
+}
+
+export default UserHome;
