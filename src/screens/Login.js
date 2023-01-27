c212f86f76bc985fb3e03646a4ce40a8b06f2c73
Tyler Jeng <93019052+TJeng7@users.noreply.github.com>
2023-01-27 16:51:19 -0500

    Logout (#3)
    * Adds logout button to the user homepage
    
    * Adds signout authentication to the logout button
    
    * add login functionality with firebase authentication
    
    * Updates wording
    
    * Adds logout button to the user homepage
    
    * Adds signout authentication to the logout button
    
    * Rebased
    
    * Updates wording
    
    * Update Login.js
    
    * Fixed merge conflicts
    
    * Changes text on logout button
    
    * Removes debugging code and updates TODO comments
    
    ---------
    
    Co-authored-by: randyoni <andykao78@gmail.com>

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,26 +1,71 @@
-import React from "react";
 import { Button, Text, View, StyleSheet, Pressable, TextInput } from 'react-native';
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 function Login({ navigation })  {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const auth = getAuth();
+
+    //Method to register new user in firebase
+    const registerNewUser = () => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                const user = userCredential.user;
+                // console.log('Successfully registered new user: ', user.email); //Debugging
+                navigation.navigate('UserHome')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    //Method to log in using an existing user
+    //MAYBE: create custom error messages when receiving error from firebase?
+    //Duplicate method for managers, change screen that it navigates to manager home
+    const loginUser = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                const user = userCredential.user;
+                // console.log('Successfully logged in with user: ', user.email); //Debugging
+                navigation.navigate('UserHome')
+            })
+            .catch(error => alert(error.message))
+    }
+
     return (
         <View>
             <View style={styles.Container}>
                 <Text style={styles.headerText}>NextGym</Text>
-                <Button
-                    title="User Home"
-                    onPress={() => navigation.navigate('UserHome')}
-                />
                 <Text style={styles.text}>Username:</Text>
-                <TextInput style={styles.input}/>
+                <TextInput //Username/Email field
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    style={styles.input}
+                />
                 <Text style={styles.text}>Password:</Text>
-                <TextInput style={styles.input} secureTextEntry />
+                <TextInput //Password field
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry //automatically turn characters into asterisks
+                />
             </View>
             <View style={styles.loginButtonContainer}>
-                <Pressable style={styles.userLogin} textStyle={styles.text}>
+                <Pressable //User login button
+                    style={styles.userLogin} 
+                    textStyle={styles.text} 
+                    onPress={loginUser}
+                >
                     <Text style={styles.text}>Login as{'\n'}User</Text>
                 </Pressable>
                 <Text style={styles.blank}></Text>
-                <Pressable style={styles.managerLogin} textStyle={styles.text}>
+                <Pressable //Manager login button
+                    style={styles.managerLogin} 
+                    textStyle={styles.text} 
+                    onPress={registerNewUser} //TODO: No register user button, so the manager button, which is not currently implemented, is temporarily being used to perform this task.
+                >
                     <Text style={styles.text}>Login as{'\n'}Manager</Text>
                 </Pressable>
             </View>
--- src/screens/UserHome.js
+++ src/screens/UserHome.js
@@ -1,24 +1,46 @@
 import React from "react";
 import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Button } from "react-native-elements";
 import EquipmentList from "../components/EquipmentList";
+import { getAuth, signOut } from "firebase/auth";
 
 // Essentially entire user home page, including welcome,
 //   streak, equipment, and buttons for working out and settings
-function UserHome() {
+function UserHome({navigation}) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.header}>Welcome,{'\n'}User</Text>
-            <Text style={styles.streak}>5 Day Streak!!</Text>
-            <Text style={styles.eaag}>Equipment at a Glance</Text>
-            <EquipmentList style={styles.el}/>
-            <Pressable style={styles.start} textStyle={styles.text}>
-                <Text style={styles.text}>Select/Start Workout</Text>
-            </Pressable>
-            <Pressable style={styles.start} textStyle={styles.text}>
-                <Text style={styles.text}>User Settings</Text>
-            </Pressable>
+        <View style = {styles.outer}>
+            <Button //Logout button. TODO: format button style
+                title="Logout"
+                onPress={() => //call method to log out user
+                    logout({navigation})
+                }
+            />
+            <View style={styles.container}>
+                <Text style={styles.header}>Welcome,{'\n'}User</Text>
+                <Text style={styles.streak}>5 Day Streak!!</Text>
+                <Text style={styles.eaag}>Equipment at a Glance</Text>
+                <EquipmentList style={styles.el}/>
+                <Pressable style={styles.start} textStyle={styles.text}>
+                    <Text style={styles.text}>Select/Start Workout</Text>
+                </Pressable>
+                <Pressable style={styles.start} textStyle={styles.text}>
+                    <Text style={styles.text}>User Settings</Text>
+                </Pressable>
+            </View>
         </View>
     );
+}
+
+//Method to sign out of firebase, returns user to login screen
+function logout( {navigation} ) {
+    const auth = getAuth();
+    // Signs out user
+    signOut(auth).then(() => {
+    //    console.log("LOGOUT"); //Debugging
+        navigation.navigate('Login');
+    }).catch((error) => {
+    //    console.log("ERROR");
+    });
 }
 
 const styles = StyleSheet.create({
@@ -63,6 +85,11 @@ const styles = StyleSheet.create({
         color: '#ebeeff',
         textAlign: 'center',
         fontSize: 35,
+    },
+    outer: {
+         flex: 1,
+
+
     }
   });
 
