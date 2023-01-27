cd5486759562e70a02f5ef211c5adccd2209686a
Leol536 <93019040+Leol536@users.noreply.github.com>
2023-01-27 15:24:48 -0500

    Adds user manager login screen #NEX-4 (#2)
    * Adds basic layout for login screen
    
    * Fixes text spillover on buttons
    
    * Fixes alignment issues
    
    * Adds basic layout for login screen
    
    * Fixes text spillover on buttons
    
    * Fixes alignment issues
    
    * Add password hider
    
    ---------
    
    Co-authored-by: Leo Lee <jlee3753@gatech.edu>

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,89 @@
 import React from "react";
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet, Pressable, TextInput } from 'react-native';
 
 function Login({ navigation })  {
     return (
         <View>
-            <Button
-                title="User Home"
-                onPress={() => navigation.navigate('UserHome')}
-            />
+            <View style={styles.Container}>
+                <Text style={styles.headerText}>NextGym</Text>
+                <Button
+                    title="User Home"
+                    onPress={() => navigation.navigate('UserHome')}
+                />
+                <Text style={styles.text}>Username:</Text>
+                <TextInput style={styles.input}/>
+                <Text style={styles.text}>Password:</Text>
+                <TextInput style={styles.input} secureTextEntry />
+            </View>
+            <View style={styles.loginButtonContainer}>
+                <Pressable style={styles.userLogin} textStyle={styles.text}>
+                    <Text style={styles.text}>Login as{'\n'}User</Text>
+                </Pressable>
+                <Text style={styles.blank}></Text>
+                <Pressable style={styles.managerLogin} textStyle={styles.text}>
+                    <Text style={styles.text}>Login as{'\n'}Manager</Text>
+                </Pressable>
+            </View>
+            <View style={styles.Container}>
+                <Pressable style={styles.forgotPassword} textStyle={styles.text}>
+                    <Text style={styles.text}>Forgot Password?</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    Container: {
+        flexDirection: 'column',
+        justfiyContent: 'center',
+        alignItems: 'center',
+    },
+    loginButtonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 15,
+    },
+    headerText: {
+        fontSize: 50,
+        textAlign: 'center',
+        paddingTop: 20,
+    },
+    input: {
+        height: 40,
+        backgroundColor: '#D9D9D9',
+        width: '90%',
+        textAlign: 'center',
+    },
+    userLogin: {
+        height: 150,
+        backgroundColor: '#D9D9D9',
+        width: '43%',
+        justifyContent: 'center',
+    },
+    blank: {
+        height: 150,
+        backgroundColor: 'FFFFFF',
+        width: '4%',
+    },
+    managerLogin: {
+        height: 150,
+        backgroundColor: '#D9D9D9',
+        width: '43%',
+        justifyContent: 'center',
+    },
+    forgotPassword: {
+        height: 80,
+        backgroundColor: '#D9D9D9',
+        width: '90%',
+        justifyContent: 'center',
+        marginTop: 15,
+    },
+    text: {
+        color: '#000000',
+        textAlign: 'center',
+        fontSize: 35,
+    },
+})
 
 export default Login;
