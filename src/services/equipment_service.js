45c969ca5d478630f5febfeaa4f48190e6330a93
randyoni <andykao78@gmail.com>
2023-02-14 22:19:58 -0500

    Adds starter code for updating and removing equipment from the equipment list

--- src/services/equipment_service.js
+++ src/services/equipment_service.js
@@ -1,5 +1,5 @@
 import { firestoredb } from "../../firebase-config"
-import { collection, doc, getDocs, setDoc } from "firebase/firestore"
+import { collection, doc, getDocs, setDoc, updateDoc, deleteField } from "firebase/firestore"
 
 const equipmentCollectionRef = collection(firestoredb, "equipment");
 
@@ -15,7 +15,26 @@ class EquipmentService {
         }
         let equipmentDoc = doc(firestoredb, 'equipment', equipmentName)
         return setDoc(equipmentDoc, equipmentData);
+    };
+
+    deleteEquipment = async (equipmentName, equipmentCount, equipmentMuscleGroups) => {
+        let equipmentData = {
+            'count': equipmentCount,
+            'muscle groups': equipmentMuscleGroups
+        }
+        let equipmentDoc = doc(firestoredb, 'equipment', equipmentName)
+        return updateDoc(equipmentDoc, {equipmentData: deleteField()});
+    };
+
+    updateEquipment = async (equipmentName, equipmentCount, equipmentMuscleGroups) => {
+    let equipmentData = {
+        'count': equipmentCount,
+        'muscle groups': equipmentMuscleGroups
     }
+    let equipmentDoc = doc(firestoredb, 'equipment', equipmentName)
+    return updateDoc(equipmentDoc, equipmentData);
+    };
+
 }
 
 export default new EquipmentService();
