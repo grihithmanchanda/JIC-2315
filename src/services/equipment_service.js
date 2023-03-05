ee82afbc873c131b5b7b31f9c5cad6cd992749b0
randyoni <andykao78@gmail.com>
2023-03-04 22:05:15 -0500

    Updates equipment management functions to pull from new database schema

--- src/services/equipment_service.js
+++ src/services/equipment_service.js
@@ -1,7 +1,9 @@
 import { firestoredb } from "../../firebase-config"
-import { collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteField, deleteDoc, listCollections } from "firebase/firestore"
+import { collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteField, deleteDoc, listCollections, arrayUnion, arrayRemove } from "firebase/firestore"
 
 const equipmentCollectionRef = collection(firestoredb, "equipment");
+
+let gymID = '';
 
 class EquipmentService {
 
@@ -10,7 +12,7 @@ class EquipmentService {
 
         // retrieve associated gym from manager account
         let managerDataSnap = await getDoc(doc(collection(firestoredb, 'managers'), currentLoginEmail))
-        let gymID = managerDataSnap.data()['gymID']
+        gymID = managerDataSnap.data()['gymID']
 
         // retrieve all equipment present at this gym
         let gymDataSnap = await getDoc(doc(collection(firestoredb, 'gym metadata'), gymID))
@@ -26,20 +28,35 @@ class EquipmentService {
     };
 
     addEquipment = async (equipmentName, equipmentCount, equipmentMuscleGroups) => {
+        // Creates the new equipment and adds it to the equipment collection
         let equipmentData = {
             'count': equipmentCount,
             'muscle groups': equipmentMuscleGroups
         }
         let equipmentDoc = doc(firestoredb, 'equipment', equipmentName)
-        return setDoc(equipmentDoc, equipmentData);
+        setDoc(equipmentDoc, equipmentData);
+
+        // Adds the reference to the equipment under the gym metadata
+        const gymdata = doc(collection(firestoredb, 'gym metadata'), gymID)
+                await updateDoc(gymdata, {
+            equipment: arrayUnion(equipmentDoc)
+        })
     };
 
     deleteEquipment = async (equipmentName) => {
+        // Deletes the equipment from equipment collection
         let equipmentDoc = doc(firestoredb, 'equipment', equipmentName)
-        return deleteDoc(equipmentDoc);
+        deleteDoc(equipmentDoc);
+        
+        // Removes the reference to the equipment
+            const gymdata = doc(collection(firestoredb, 'gym metadata'), gymID)
+                await updateDoc(gymdata, {
+            equipment: arrayRemove(equipmentDoc)
+        })
     };
 
     updateEquipment = async (equipmentName, equipmentCount, equipmentMuscleGroups) => {
+        // Updates the equipment in the equipment collection
         let equipmentData = {
             'count': equipmentCount,
             'muscle groups': equipmentMuscleGroups
