777f3544ed8e7c5e01daca0ccc2d2e57b983ba67
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2023-04-02 20:03:49 -0400

    Makes userhome a scrollview

--- src/screens/UserHome.js
+++ src/screens/UserHome.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, ScrollView, View } from "react-native";
 import { Button } from "react-native-elements";
 import EquipmentList from "../components/EquipmentList";
 import { getAuth, signOut } from "firebase/auth";
@@ -9,50 +9,52 @@ import styles from "../styles/styles";
 // streak, equipment, and buttons for working out and settings
 function UserHome({navigation}) {
 
-  const handleGymInformation = () => {
-    navigation.navigate('GymInformation');
-  };
+    const handleGymInformation = () => {
+        navigation.navigate('GymInformation');
+    };
 
-  return (
-    <View style={styles.outer}>
-      <Button //Logout button. TODO: format button style
-        title="Logout"
-        onPress={() =>
-          //call method to log out user
-          logout({ navigation })
-        }
-      />
-      <View style={styles.container}>
-        <Text style={styles.header}>Welcome,{"\n"}User</Text>
-        <Text style={styles.subheader}>5 Day Streak!</Text>
-        <Text style={styles.equipmentContainer}>Equipment at a Glance</Text>
-        <EquipmentList style={styles.equipmentList} />
-        <Pressable style={styles.button} textStyle={styles.text} onPress={() => {navigation.navigate('WOTDInfo')}}>
-          <Text style={styles.text}>Start Workout</Text>
-        </Pressable>
-        <Pressable style={styles.button} textStyle={styles.text}>
-          <Text style={styles.text}>User Settings</Text>
-        </Pressable>
-        <Pressable style={styles.button} textStyle={styles.text} onPress={() => handleGymInformation()}>
-          <Text style={styles.text}>Gym Information</Text>
-        </Pressable>
-      </View>
-    </View>
-  );
+    return (
+        <ScrollView style={styles.outer}>
+            <Button //Logout button. TODO: format button style
+                title="Logout"
+                onPress={() =>
+                    //call method to log out user
+                    logout({navigation})
+                }
+            />
+            <View style={styles.container}>
+                <Text style={styles.header}>Welcome,{"\n"}User</Text>
+                <Text style={styles.subheader}>5 Day Streak!</Text>
+                <Text style={styles.equipmentContainer}>Equipment at a Glance</Text>
+                <EquipmentList style={styles.equipmentList}/>
+                <Pressable style={styles.button} textStyle={styles.text} onPress={() => {
+                    navigation.navigate('WOTDInfo')
+                }}>
+                    <Text style={styles.text}>Start Workout</Text>
+                </Pressable>
+                <Pressable style={styles.button} textStyle={styles.text}>
+                    <Text style={styles.text}>User Settings</Text>
+                </Pressable>
+                <Pressable style={styles.button} textStyle={styles.text} onPress={() => handleGymInformation()}>
+                    <Text style={styles.text}>Gym Information</Text>
+                </Pressable>
+            </View>
+        </ScrollView>
+    );
 }
 
 //Method to sign out of firebase, returns user to login screen
-function logout({ navigation }) {
-  const auth = getAuth();
-  // Signs out user
-  signOut(auth)
-    .then(() => {
-      // console.log("LOGOUT"); //Debugging
-      navigation.navigate("Login");
-    })
-    .catch((error) => {
-      // console.log("ERROR");
-    });
+function logout({navigation}) {
+    const auth = getAuth();
+    // Signs out user
+    signOut(auth)
+        .then(() => {
+            // console.log("LOGOUT"); //Debugging
+            navigation.navigate("Login");
+        })
+        .catch((error) => {
+            // console.log("ERROR");
+        });
 }
 
 export default UserHome;
