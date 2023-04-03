d77f455a5fe83a82966afe308b9780e992fbae5a
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2023-04-03 12:08:23 -0400

    Adds initial user WOD backend code
    Co-Authored-By: Tyler Jeng <93019052+TJeng7@users.noreply.github.com>

--- src/services/workout_service.js
+++ src/services/workout_service.js
@@ -1,6 +1,9 @@
 import { firestoredb } from "../../firebase-config"
-import { collection, doc, setDoc, updateDoc } from "firebase/firestore"
+import { collection, doc, getDoc, setDoc, updateDoc } from "firebase/firestore"
 import equipment_service from "./equipment_service";
+
+
+const difficultyMapping = {0: 'novice', 1:'intermediate', 2:'advanced'};
 
 class WorkoutService {
 
@@ -29,14 +32,8 @@ class WorkoutService {
         let gymDoc = doc(firestoredb, 'gym metadata', gymID)
         let workoutsCollectionRef = collection(gymDoc, 'workouts')
 
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const date = `${month}-${day}-${year}`;
-
-        let difficultyMapping = {0: 'novice', 1:'intermediate', 2:'advanced'}
-        let difficultyLevelText = difficultyMapping[workoutDifficulty];
+        const date = this.getDateString();
+        const difficultyLevelText = difficultyMapping[workoutDifficulty];
 
         // This arrangement can be altered based on how we want the date's format to appear.
         let wotdDocRef = doc(workoutsCollectionRef, date)
@@ -45,9 +42,36 @@ class WorkoutService {
             [difficultyLevelText]: workoutList
         }
 
-        let wotdDoc = await updateDoc(wotdDocRef, wotdData)
+        let wotdDoc = await setDoc(wotdDocRef, wotdData, { merge: true })
 
         return wotdDoc
+
+    }
+
+    // gets current date, formatted as padded MM-DD-YYYY
+    getDateString() {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const date = `${month}-${day}-${year}`;
+        return date;
+    }
+
+// gets Workout of the Day for the current day
+    getWOD = async (workoutDifficulty = 0) => {
+        let gymDoc = doc(firestoredb, 'gym metadata', global.gymID)
+        let workoutsCollectionRef = collection(gymDoc, 'workouts')
+
+        const currentDate = this.getDateString();
+        const difficultyLevelText = difficultyMapping[workoutDifficulty];
+
+        let wodDocRef = doc(workoutsCollectionRef, currentDate)
+        let wodDoc = await getDoc(wodDocRef)
+        let wodData = wodDoc.data()
+
+        // format: {"difficulty1": ['exercise1', 'exercise2', ...], "difficulty2": [...], ...}
+        return wodData
 
     }
 }
